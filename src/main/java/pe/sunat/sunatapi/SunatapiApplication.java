9087e408baf49360df9504309a25583b7d9e8421
mauri <mauri@LAPTOPDEMAU>
2021-09-02 16:34:01 -0500

    App Sunat start

---
+++ src/main/java/pe/sunat/sunatapi/SunatapiApplication.java
@@ -0,0 +1,13 @@
+package pe.sunat.sunatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunatapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/sunat/sunatapi/SunatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.sunat.sunatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
